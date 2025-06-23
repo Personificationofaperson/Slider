908d2ebd89007196a0f64d87a5b51d2e088d7035
Personificationofaperson <verbeeck.kimberly@hotmail.com>
2025-06-23 20:32:26 +0200

    Loopable slider

--- arrow.js
+++ arrow.js
@@ -2,32 +2,30 @@ const leftArrow = document.getElementById("arrow-left");
 const rightArrow = document.getElementById("arrow-right");
 const allSlides = document.querySelectorAll(".slide");
 const carrousel = document.querySelector(".carrousel");
-const slideWidth = document.querySelector(".slide").offsetWidth;
+const gap = parseFloat(getComputedStyle(carrousel).gap); // get exact gap value from CSS, default to 32px if not set
+const slideWidth = document.querySelector(".slide").offsetWidth + gap + 20; //full width of each slide including gap and margin set in CSS
 let index = 0;
 
         leftArrow.addEventListener("click", function(e) {
-                index--;
+                index --;
                 console.log(index);
                 if (index === -1) {
-                        carrousel.style.transform = `translateX(-${slideWidth * allSlides.length}px)`;
-                        index = allSlides.length;
+                        carrousel.style.transform = `translateX(-${slideWidth * 3}px)`;
+                        index = 3;
                 }
                 else if (index <= allSlides.length) {
-                        console.log("slidewidth ", slideWidth, index);
                         carrousel.classList.add("moveslider");  
                         carrousel.style.transform = `translateX(-${slideWidth * index}px)`;
                 }
         });
 
-        //if there's x slides to scroll, scroll that amount of slides
-
     rightArrow.addEventListener("click", function(e) {
-        index++;
-        if (index <= allSlides.length) {
+        index ++;
+        if (index <= allSlides.length - 3) {
                 carrousel.classList.add("moveslider");
                 carrousel.style.transform = `translateX(-${slideWidth * index}px)`;
         }
-        else if (index >= allSlides.length) {
+        else  {
                 carrousel.style.transform = "translateX(0px)";
                 index = 0;
         }
