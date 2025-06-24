9d92f0d3f9d64c62d4312ffdd890cec7ec27e6f7
Personificationofaperson <verbeeck.kimberly@hotmail.com>
2025-06-24 20:02:42 +0200

    Scalable logic: scroll slidewidth with last click showing full last slide

--- arrow.js
+++ arrow.js
@@ -2,18 +2,25 @@ const leftArrow = document.getElementById("arrow-left");
 const rightArrow = document.getElementById("arrow-right");
 const allSlides = document.querySelectorAll(".slide");
 const carrousel = document.querySelector(".carrousel");
-const gap = parseFloat(getComputedStyle(carrousel).gap); // get exact gap value from CSS, default to 32px if not set
-const slideWidth = document.querySelector(".slide").offsetWidth + gap + 20; //full width of each slide including gap and margin set in CSS
-let index = 0;
+const slideWidth = document.querySelector(".slide").offsetWidth + parseFloat(getComputedStyle(carrousel).gap) + 20; //full width of each slide including gap and margin set in CSS
+
+const widthCarrousel = document.querySelector(".outer-carrousel").scrollWidth;
+const initialSlidesInView = Math.round(document.querySelector(".outer-carrousel").offsetWidth / slideWidth); // how many slides are in view at the start
+const maxIndex = allSlides.length - initialSlidesInView; // maximum index to scroll to, which is the total number of slides minus the number of slides in view at the start
+const widthInitial = document.querySelector(".outer-carrousel").offsetWidth; // width of the outer carrousel, including the remaining scrollwidth
+const fullScrollAmount = Math.round((widthCarrousel - widthInitial) / initialSlidesInView + 10); //amount to scroll per click, based on the width of the outer carrousel and the number of slides in view with extra padding for the last slide
+
+index = 0;
 
         leftArrow.addEventListener("click", function(e) {
                 index --;
                 console.log(index);
-                if (index === -1) {
-                        carrousel.style.transform = `translateX(-${slideWidth * 3}px)`;
-                        index = 3;
+                if (index < 0) { // if you click the left arrow on the first slide, position is reset to the last slide for looping behaviour
+                        carrousel.style.transform = `translateX(-${fullScrollAmount * maxIndex}px)`; // last slide position
+                        carrousel.classList.add("moveslider");  
+                        index = maxIndex; 
                 }
-                else if (index <= allSlides.length) {
+                else if (index <= maxIndex) {
                         carrousel.classList.add("moveslider");  
                         carrousel.style.transform = `translateX(-${slideWidth * index}px)`;
                 }
@@ -21,12 +28,16 @@ let index = 0;
 
     rightArrow.addEventListener("click", function(e) {
         index ++;
-        if (index <= allSlides.length - 3) {
+        if (index < maxIndex) { // if index is smaller than 3 (maxindex), we can scroll
                 carrousel.classList.add("moveslider");
                 carrousel.style.transform = `translateX(-${slideWidth * index}px)`;
         }
-        else  {
-                carrousel.style.transform = "translateX(0px)";
-                index = 0;
+        else if (index === maxIndex)  { //if index is equal to 3, we can scroll to the last slide
+                carrousel.style.transform = `translateX(-${fullScrollAmount * index}px)`; // if index is equal to 3, we can scroll to the last slide
         }
+        else {
+                carrousel.style.transform = "translateX(0px)"; // if index is greater than 3, we reset the position to the first slide
+                index = 0; // reset index to 0
+        }
+
 });
